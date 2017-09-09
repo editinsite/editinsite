f221b4d1247eae1a5a9108cee5deffa0094e4b4c
jwoy <jwoyame@usgeo.com>
2017-09-09 17:48:02 -0400

    import github.com/jwoy/vagrant-ubuntu-golang

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GOSOURCE=https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+GOTARGET=/usr/local
+PROFILE=/home/ubuntu/.profile
+
+# download Go tools to $GOTARGET/go
+curl -sSL $GOSOURCE -o /tmp/go.tar.gz
+tar -xf /tmp/go.tar.gz -C $GOTARGET
+rm /tmp/go.tar.gz
+
+# apply Go environment configuration to the user's .profile
+printf "\n" >> $PROFILE
+printf "# golang configuration\n" >> $PROFILE
+printf "export GOROOT=$GOTARGET/go\n" >> $PROFILE
+printf "export GOPATH=/vagrant/go\n" >> $PROFILE
+printf "export PATH=\$PATH:$GOTARGET/go/bin:\$GOPATH/bin\n" >> $PROFILE
