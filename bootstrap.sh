f11a0c05d04ede6e8ce1fa5b3c0d128253c16f0a
jwoy <jwoyame@usgeo.com>
2017-09-18 17:12:34 -0400

    Add Monaco web editor

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+NODESOURCE=https://deb.nodesource.com/setup_6.x
 GOSOURCE=https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 GOTARGET=/usr/local
 PROFILE=/home/ubuntu/.profile
 
+export DEBIAN_FRONTEND=noninteractive
+
 # download Go tools to $GOTARGET/go
+echo "Installing Golang..."
 curl -sSL $GOSOURCE -o /tmp/go.tar.gz
 tar -xf /tmp/go.tar.gz -C $GOTARGET
 rm /tmp/go.tar.gz
+
+# Use most recent stable Node.js
+echo "Registering Node.js source..."
+curl -sSL $NODESOURCE -o /tmp/nodesource_setup.sh
+bash /tmp/nodesource_setup.sh > /dev/null
+rm /tmp/nodesource_setup.sh
+
+apt-get -qq update > /dev/null
+
+# Now install packages
+echo "Installing Node.js..."
+apt-get -qq install nodejs > /dev/null
 
 # apply Go environment configuration to the user's .profile
 printf "\n" >> $PROFILE
@@ -22,6 +38,7 @@ chown ubuntu:ubuntu /home/ubuntu/src/github.com/editinsite
 
 # switch to the "ubuntu" user to do the build
 # run the "export"s in .profile to apply to current session
+echo "Compiling EditInsite server..."
 su ubuntu -c "source ~/.profile && go install github.com/editinsite/editinsite"
 
 # check for either directory or symlink to "ui" directory
@@ -33,6 +50,7 @@ if ! [ -d /home/ubuntu/bin/editinsite.json ]; then
 fi
 
 # now daemonize the server
+echo "Starting server..."
 DAEMON=/lib/systemd/system/editinsite.service
 printf "[Unit]\nDescription=EditInsite server\n\n" >> $DAEMON
 printf "[Service]\nType=simple\nUser=ubuntu\nGroup=ubuntu\n" >> $DAEMON
--- ui/script.js
+++ ui/script.js
@@ -1,4 +1,9 @@
-var _currProject, _currFile;
+"use strict";
+
+var _currProject, _currFile,
+    _editor;
+
+require.config({ paths: { 'vs': 'monaco-editor/min/vs' }});
 
 $(document).ready(function () {
   registerEvents();
@@ -7,18 +12,19 @@ $(document).ready(function () {
 
 function registerEvents () {
   $('#files').on('click', 'a', fileNameClick);
-  $('#editor').submit(saveFile);
+  $('input[type=submit]').click(saveFile);
+  $(window).resize(onResize);
 }
 
 function getProjectList () {
-  $.getJSON("/projects", function (projects) {
+  $.getJSON('/projects/', function (projects) {
     _currProject = projects[0];
     getFileList();
   });
 }
 
 function getFileList () {
-  $.getJSON("/projects/" + _currProject.id, function (files) {
+  $.getJSON('/projects/' + _currProject.id, function (files) {
     var $list = $('#files');
     for (var i = 0; i < files.length; i++)
       $list.append('<a href="#">' + files[i] + '</a>');
@@ -29,26 +35,140 @@ function fileNameClick (e) {
   e.preventDefault();
 
   var $fileLink = $(this);
-  $fileLink.addClass('selected').siblings().removeClass('selected')
-  var name = $fileLink.text();
+  if (!$fileLink.hasClass('selected')) {
+    $fileLink.addClass('selected').siblings().removeClass('selected')
+    var name = $fileLink.text();
+    loadFile(name);
+  }
+}
+
+function loadFile (path) {
   _currFile = null;
-  $.getJSON("/projects/" + _currProject.id + "/" + name, showFile);
+  showLoading('editor');
+  $.getJSON('/projects/' + _currProject.id + '/' + path,
+    function (file) {
+      require(['vs/editor/editor.main'], function() {
+        file.type = detectFileType(file);
+        showFile(file);
+      });
+    })
+    .fail(function() { showFile(null); });
 }
 
 function showFile (file) {
   _currFile = file;
-  $('textarea').val(file.body);
+
+  require(['vs/editor/editor.main'], function() {
+    if (!file) {
+      if (_editor) {
+        if (_editor.getModel()) {
+          _editor.getModel().dispose();
+        }
+        _editor.dispose();
+        _editor = null;
+      }
+      hideLoading('editor');
+      $('#editor').empty();
+      $('#editor').append('<p class="alert">File could not be loaded.</p>');
+      return;
+    }
+  
+    if (!_editor) {
+      $('#editor').empty();
+      _editor = monaco.editor.create(document.getElementById('editor'), {
+        model: null,
+        theme: 'vs-dark'
+      });
+    }
+
+    var oldModel = _editor.getModel();
+    var newModel = monaco.editor.createModel(file.body, file.type.id);
+    _editor.setModel(newModel);
+    if (oldModel) {
+      oldModel.dispose();
+    }
+    hideLoading('editor');
+  });
 }
 
 function saveFile (e) {
   e.preventDefault();
 
-  $('input[type=submit]').text('Saving...');
-  $.post("/projects/" + _currProject.id + "/" + _currFile.path, {
-      body: $('textarea').val()
-    },
-    function (files) {
-      $('input[type=submit]').text('Save');
-    },
-    "json");
+  if (_editor) {
+    $('input[type=submit]').text('Saving...');
+    $.post("/projects/" + _currProject.id + "/" + _currFile.path, {
+        body: _editor.getValue()
+      },
+      function (files) {
+        $('input[type=submit]').text('Save');
+      },
+      'json');
+  }
+}
+
+function detectFileType (file) {
+  var ext, extStart = file.name.lastIndexOf('.');
+  if (extStart !== -1) {
+    ext = file.name.slice(extStart).toLowerCase();
+  }
+  var exts = _extensions || loadExtensions();
+  return ext && exts[ext] || exts['.txt'];
+}
+
+var _extensions;
+function loadExtensions () {
+  _extensions = {};
+  var languages = monaco.languages.getLanguages();
+  for (var l = languages.length; l--;) {
+    var language = languages[l];
+    for (var e = language.extensions.length; e--;) {
+      var ext = language.extensions[e];
+      _extensions[ext] = language;
+    }
+  }
+  return _extensions;
+}
+
+function onResize () {
+  if (_editor) {
+    _editor.layout();
+  }
+}
+
+function showLoading (className) {
+  var $el = $('.loading.' + className);
+  $el.find('.progress').hide();
+  $el.show();
+
+  if (!$el.data('delayFn')) {
+    $el.data('delayFn', debounce(function () {
+      if ($el.is(':visible'))
+        $el.find('.progress').show();
+    }, 250));
+  }
+  $el.data('delayFn')();
+}
+
+function hideLoading (className) {
+  $('.loading.' + className).fadeOut({ duration: 200 });
+}
+
+// Returns a function that can be used to call func() on a delay.
+// If `immediate` is true, func() is called on leading rather than trailing edge.
+function debounce (func, delayMs, immediate) {
+  var timer;
+  return function () {
+    var context = this,
+        args = arguments;
+    function later () {
+      timer = null;
+      if (!immediate) func.apply(context, args);
+    }
+    var callNow = immediate && !timer;
+    clearTimeout(timer);
+    timer = setTimeout(later, delayMs);
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
 }
