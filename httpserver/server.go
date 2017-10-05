2278ca15782b9ee2db32a0488c80ef5726ff9414
jwoy <jwoyame@usgeo.com>
2017-10-04 23:59:12 -0400

    Improve directory traversal + security

--- httpserver/server.go
+++ httpserver/server.go
@@ -1,4 +1,4 @@
-// HTTP server for EditInsite
+// Package httpserver provides the HTTP server for the EditInsite pages and API.
 package httpserver
 
 import (
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/editinsite/editinsite/config"
@@ -13,32 +14,72 @@ import (
 )
 
 var dirListTemplate = template.Must(template.New("dir").Parse(`<!doctype html>
-	<html>
-	<head>
-	  <meta charset="utf-8">
-	  <title>Directory listing</title>
-	</head>
-	<body>
-	  <ul>
-	  	{{range .}}<li><a href="{{.}}">{{.}}</a></li>{{end}}
-	  </ul>
-	</body>
-	</html>`))
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Directory listing</title>
+</head>
+<body>
+	<ul>
+	{{range .}}<li><a href="{{.}}">{{.}}</a></li>{{end}}
+	</ul>
+</body>
+</html>`))
+
+var projectListTemplate = template.Must(template.New("project").Parse(`<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Project listing</title>
+</head>
+<body>
+	<ul>
+	{{range .}}<li><a href="/projects/{{.Name}}/">{{.Name}}</a></li>{{end}}
+	</ul>
+</body>
+</html>`))
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	path := "ui/index.html"
+	info, err := os.Stat(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	http.ServeContent(w, r, info.Name(), info.ModTime(), file)
+}
 
 func projectHandler(w http.ResponseWriter, r *http.Request) {
 	if len(r.URL.Path) > len("/projects/") {
 		fileHandler(w, r)
 	} else {
 		list := projects.SortByID()
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(list)
+		accept := r.Header.Get("Accept")
+		var err error
+		if r.Method == "POST" && strings.Contains(accept, "application/json") {
+			// for security, don't allow a JSON array response to a GET.
+			w.Header().Set("Content-Type", "application/json")
+			err = json.NewEncoder(w).Encode(list)
+		} else {
+			err = projectListTemplate.Execute(w, list)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
 func fileHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[len("/projects/"):]
 	if strings.Contains(path, "..") {
-		// for security
+		// prevent directory traversal attacks
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
@@ -83,7 +124,8 @@ func listHandler(w http.ResponseWriter, r *http.Request, p *projects.Workspace, 
 		return
 	}
 	accept := r.Header.Get("Accept")
-	if strings.Contains(accept, "application/json") {
+	if r.Method == "POST" && strings.Contains(accept, "application/json") {
+		// for security, don't allow a JSON array response to a GET.
 		w.Header().Set("Content-Type", "application/json")
 		err = json.NewEncoder(w).Encode(list)
 	} else {
@@ -110,9 +152,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request, p *projects.Workspace, 
 	}
 }
 
+// Start will begin serving the editinsite pages and API.
 func Start() error {
 	port := fmt.Sprintf(":%d", config.Values.Port)
 	http.HandleFunc("/projects/", projectHandler)
+	http.HandleFunc("/edit/", editHandler)
 	http.Handle("/", http.FileServer(http.Dir("ui")))
 	return http.ListenAndServe(port, nil)
 }
