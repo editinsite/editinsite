867993bc286651786db510a09e8bd179ab484694
jwoy <jwoyame@usgeo.com>
2017-10-15 23:43:33 -0400

    Load preview into frame that can run JS

--- ui/view-preview.js
+++ ui/view-preview.js
@@ -14,10 +14,20 @@ views.preview = {
 function openPath (path) {
 	var file = new ProjectFile("index.html");
 	file.download(function (file) {
+		var content = file.body.toLowerCase(),
+			bodyStart = content.indexOf('<body'),
+			bodyEnd = content.lastIndexOf('body>');
+		if (bodyStart === -1 || bodyEnd === -1) {
+			alert('Page does not have a properly-formed body.');
+			return;
+		}
+		bodyStart = content.indexOf('>', bodyStart)+1;
+		bodyEnd += 5;
+
 		// IE uses contentWindow.document
 		var iframe = document.getElementById('preview-frame'),
 			doc = iframe.contentDocument || iframe.contentWindow.document;
-		doc.getElementsByTagName('html')[0].innerHTML = file.body;
+		doc.getElementsByTagName('div')[0].innerHTML = file.body.slice(bodyStart, bodyEnd);
 	});
 }
 
