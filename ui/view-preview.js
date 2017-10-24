da2d3144e918a1c2d2a061201f27848fe6e43665
jwoy <jwoyame@usgeo.com>
2017-10-23 21:31:39 -0400

    Use <base> for relative URLs in preview

--- ui/view-preview.js
+++ ui/view-preview.js
@@ -14,7 +14,18 @@ views.preview = {
 function openPath (path) {
 	var file = new ProjectFile("index.html");
 	file.download(function (file) {
-		var content = file.body;
+		var content = file.body,
+			contentL = content.toLowerCase(),
+			headStart = contentL.indexOf('<head');
+		if (headStart === -1) {
+			alert('Page does not have a properly-formed <head>.');
+			return;
+		}
+		headStart = contentL.indexOf('>', headStart)+1;
+
+		content = content.slice(0, headStart)
+			+ '<base href="' + window.location.href.replace('/preview/', '/run/') + '">'
+			+ content.slice(headStart);
 
 		// IE uses contentWindow.document
 		var iframe = document.getElementById('preview-frame'),
